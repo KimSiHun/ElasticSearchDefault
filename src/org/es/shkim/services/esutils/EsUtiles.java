e5cdb27504f3361d2e981f6bbdfe173723d6d155
KimSiHun <paradaise110@hanmail.net>
2017-05-11 17:58:08 +0900

    chk1

---
+++ src/org/es/shkim/services/esutils/EsUtiles.java
@@ -0,0 +1,20 @@
+package org.es.shkim.services.esutils;
+
+import org.elasticsearch.client.transport.TransportClient;
+
+public class EsUtiles
+{
+	public static boolean index_isExist(TransportClient client, String index)
+	{
+		return client.admin().indices().prepareExists(index).get().isExists();
+	}
+	
+	public static boolean mapping_isExist(TransportClient client, String index, String type)
+	{
+		if (!index_isExist(client, index))
+		{
+			return false;
+		}
+		return client.admin().indices().prepareTypesExists(index).setTypes(type).get().isExists();
+	}
+}
---
+++ src/org/es/shkim/services/mappings/Mappings.java
@@ -0,0 +1,10 @@
+package org.es.shkim.services.mappings;
+
+import org.elasticsearch.client.transport.TransportClient;
+
+public class Mappings
+{
+	public void create_mapping(TransportClient client, String index, String type){
+		
+	}
+}
